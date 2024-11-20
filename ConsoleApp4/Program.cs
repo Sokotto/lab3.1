ac0033b0685d5f8192f6e4261285885c81b45a25
Xenzey <Xenzey@209-3>
2024-11-20 10:45:03 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,13 @@
+﻿int lowerBound = -500;
+int upperBound = 500;
+Console.WriteLine($"Ищем трехзначные числа с четными сотнями в диапозоне от {lowerBound} {upperBound}");
+for (int i = lowerBound; i <= upperBound; i++)
+{
+    if (i >= 100 && i <= 999)
+    {
+        if ((i / 100) % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
